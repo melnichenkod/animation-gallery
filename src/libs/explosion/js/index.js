2ccc36b2268a00f9eed0fa16d14301d17c37fc92
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2022-01-18 23:37:53 +0200

    added throttle function

--- src/libs/explosion/js/index.js
+++ src/libs/explosion/js/index.js
@@ -64,3 +64,26 @@ function fadeOut(element, callback) {
     }
   }
 }
+
+function throttle(callback, delay = 200) {
+  let isWaiting = false;
+  let savedArgs = null;
+  let savedThis = null;
+  return function wrapper(...args) {
+    if (isWaiting) {
+      savedArgs = args;
+      savedThis = this;
+      return;
+    }
+    callback.apply(this, args);
+    isWaiting = true;
+    setTimeout(() => {
+      isWaiting = false;
+      if (savedThis) {
+        wrapper.apply(savedThis, savedArgs);
+        savedThis = null;
+        savedArgs = null;
+      }
+    }, delay)
+  }
+}
