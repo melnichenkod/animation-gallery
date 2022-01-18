da17456fcc193b8a88385ed08140a2f24e3c8ff9
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2022-01-18 18:54:06 +0200

    added the necessary consts

---
+++ src/libs/explosion/js/index.js
@@ -0,0 +1,32 @@
+const defaultOptions = {
+  linkClass: '.cardLink',
+  linkImageClass: '.cardImage'
+};
+
+const explosionClassName = 'explosion';
+const explosionOpenedClassName = 'explosion_Opened';
+const explosionOpeningClassName = 'explosion_Opening';
+
+const explosionSummaryClassName = 'explosionSummary';
+const explosionControlsClassName = 'explosionControls';
+const explosionImagesClassName = 'explosionImages';
+
+const explosionSummaryContentClassName = 'explosionSummaryContent';
+const explosionTitleClassName = 'explosionTitle';
+const explosionDescriptionClassName = 'explosionDescription';
+const explosionImageClassName = 'explosionImage';
+
+const explosionCloseClassName = 'explosionClose';
+const explosionNavsClassName = 'explosionsNavs';
+
+const explosionNavClassName = 'explosionNav';
+const explosionNavPrevClassName = 'explosionNavPrev';
+const explosionNavNextClassName = 'explosionNavNext';
+const explosionCounterClassName = 'explosionCounter';
+const explosionNavDisabledClassName = 'explosionNavDisabled';
+
+const explosionPrevHiddenImageClassName = 'explosionImage_PrevHidden';
+const explosionPrevShowingImageClassName = 'explosionImage_PrevShowing';
+const explosionActiveImageClassName = 'explosionImage_Active';
+const explosionNextShowingImageClassName = 'explosionImage_NextShowing';
+const explosionNextHiddenImageClassName = 'explosionImage_NextHidden'
